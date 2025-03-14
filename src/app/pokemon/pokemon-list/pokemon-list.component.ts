af33c2e5695aee72d288170cc3c4fb0e459b365d
jklzz02 <jacopo.lazzaro.jl@gmail.com>
2025-03-14 21:01:50 +0100

    Initial logic for CACHE feature brancg

--- src/app/pokemon/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon/pokemon-list/pokemon-list.component.ts
@@ -11,42 +11,77 @@ import { Subject } from 'rxjs';
 })
 export class PokemonListComponent {
 
+  private cache:IPokemonList[] = [];
+  private cacheStart:number = 0;
+  private cacheCount:number = 0;
   title = 'Pokedex';
   pokemons: IPokemonList[] = [];
   start: number = 0;
   chunk: number = 12;
   loading: boolean = true;
-  pokemonRefreshSubject = new Subject<void>();
+  pokemonRefreshSubject = new Subject<boolean>();
 
   constructor(private pokemonservice: PokemonService) {}
 
   ngOnInit() {
     this.pokemonRefreshSubject.subscribe({
-      next: () => {
+      next: (forward:boolean) => {
         this.loading = true;
-        this.loadPokemons();
+        this.loadPokemons(forward);
       },
     });
-    this.pokemonRefreshSubject.next();
+    this.pokemonRefreshSubject.next(true);
   }
 
   next() {
     this.start += 12;
-    this.pokemonRefreshSubject.next();
+    this.pokemonRefreshSubject.next(true);
   }
 
   previous() {
     if (this.start < 12) return;
     this.start -= 12;
-    this.pokemonRefreshSubject.next();
+    this.pokemonRefreshSubject.next(false);
   }
 
-  loadPokemons() {
+  loadPokemons(forward:boolean) {
 
+    /**
+     * TO DO
+     * change logic to adapt to thw concat version of the cache
+     */
+    if(this.cacheCount > 0) {
+
+      if(forward){
+        if(!((this.cacheStart + this.chunk) > this.cache.length))
+        {
+          this.cacheStart += this.chunk;
+        }
+        this.pokemons = this.cache.slice(this.cacheStart, this.cacheStart + this.chunk);
+      }else{
+
+        if(!((this.cacheStart - this.chunk) > this.cache.length))
+        {
+            this.cacheStart -= this.chunk;
+        }
+        this.pokemons = this.cache.slice(this.cacheStart, this.cacheStart + this.chunk);
+      }
+
+      this.loading = false;
+      --this.cacheCount;
+      return;
+    }
+
+    this.cacheCount = 5;
     this.pokemonservice
-      .getPokemonRange(this.start, this.chunk)
+      .getPokemonRange(this.start, this.chunk*5)
       .subscribe((data) => {
-        this.pokemons = data
+        /**
+         * TO DO
+         * try to use concat, the cache needs to grow instead of being overwritten
+         */
+        this.cache = data
+        this.pokemons = this.cache.slice(this.cacheStart, this.cacheStart + this.chunk)
         this.loading = false;
       });
   }
